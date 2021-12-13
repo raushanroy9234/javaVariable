18703510b479724dfea37c5768e8d9e10a6b1aa2
Raushan roy <Raushanroyece062@gmail.com>
2021-12-13 15:18:18 +0530

    First commit

---
+++ LocalGlobal.java
@@ -0,0 +1,35 @@
+
+
+class LocalGlobal{
+    static String str = "make my machine";//Global variable
+    public static void main(String[] args) {
+        int x = 10;//local variable
+        System.out.println(x);
+        System.out.println(str);
+
+        {
+            int y = 20;//local variable
+            System.out.println(y);
+            System.out.println(str);
+        }
+        {
+            int j = 890;//local variable
+            System.out.println(j);
+            System.out.println(str);
+        }
+        {
+            int j = 676;//local variable
+            j = 5550;
+            System.out.println(j);
+            System.out.println(j);
+        }
+        
+        
+    
+    }
+   
+   
+    
+
+   
+}
